dbf820ddece92760ca3f344c1e3eb3cd0530cbb4
ravi11o <ravi.suraj110@gmail.com>
2022-06-22 12:18:44 +0530

    string method

--- block-BJaadc/code/index.js
+++ block-BJaadc/code/index.js
@@ -5,7 +5,7 @@ console.log(`The length of ${message} is ${message.length}` )
 // Log the character at index 0 of the variable message like `The first character of [message] is [character]`
 console.log(`The first charcter of ${message} is ${message.charAt(0)}`)
 // Log the last character of message using length like `The last character of [message] is [character]`
-console.log(`The last character of ${message} is ${message.charAt(41)}`)
+console.log(`The last character of ${message} is ${message.charAt(message.length-1)}`)
 // Log the index of word "you" in message
 console.log(`${message.indexOf("you")}`)
 // Log the index of word "still" in message
@@ -20,6 +20,7 @@ console.log(`${message.includes("answer")}`)
 // Log true or false based on whether the word "they" exist in message or not
 console.log(`${message.includes("they")}`)
 
+
 // Log true or false based on whether the word "is" exist in message or not
 console.log(`${message.includes("is")}`)
 
@@ -28,13 +29,13 @@ console.log(`${message.includes("Is")}`)
 
 // Check and log whether the word from index 3 to 6 is "you" or not (use slice)
 message.slice(3,6);
-console.log(`${message.slice(3,6)}`)
+console.log(`${message.slice(3,6)=="you"}`)
 // Check and log whether the word from index 7 to 8 is "a" or not
-console.log(`${message.slice(7,8)}`)
+console.log(`${message.slice(7,8)=="a"}`)
 // Check and log whether the last 3 character in message is "no!" or not (you can use -3 in slice)
-console.log(`${message.slice(39,43)}`)
+console.log(`${message.slice(-3)==="no!"}`)
 // Check and log whether the last 5 character in message is "no!" or not
-console.log(`${message.slice(37,43)}`)
+console.log(`${message.slice(-5)==="no!"}`)
 // Log the message variable, all in lowecase
 console.log(`${message.toLocaleLowerCase()}`)
 // Log the message variable, all in uppercase
@@ -55,8 +56,8 @@ for(let char of message){
   console.log(char)
 }
 // Split all the words in message (split by " " space) and store it in a variable messageArray
-console.log(`${message.split(" ")}`)
+let messageArray=message.split(" ")
 // Log all the words of messageArray
-for(let words of message){
-  console.log(words)
+for(let word of messageArray){
+  console.log(word)
 }
